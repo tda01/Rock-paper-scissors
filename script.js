8080c6bc96db720fd572c905e318902828e83277
Daniel <trifdaniel01@gmail.com>
2021-10-31 15:01:52 +0200

    Updated UI

--- script.js
+++ script.js
@@ -8,37 +8,37 @@ function playRound(playerSelection, computerSelection) {
     
     if (playerSelection === computerSelection) {    
         return "It's a tie!";
-    } else if ((playerSelection === "paper" || computerSelection === "paper") && (playerSelection === "rock" || computerSelection === "rock")) {
-        if (playerSelection === "paper") {
+    } else if ((playerSelection === btns[1] || computerSelection === "paper") && (playerSelection === btns[0] || computerSelection === "rock")) {
+        if (playerSelection === btns[1]) {
             return "You win! Paper beats rock";
         } else {
             return "You lose! Paper beats rock";
         }
-    } else if ((playerSelection === "paper" || computerSelection === "paper") && (playerSelection === "scissors" || computerSelection === "scissors")) {
-        if (playerSelection === "scissors") {
+    } else if ((playerSelection === btns[1] || computerSelection === "paper") && (playerSelection === btns[2] || computerSelection === "scissors")) {
+        if (playerSelection === btns[2]) {
             return "You win! Scissors beat paper";
         } else {
             return "You lose! Scissors beat paper";
         }
-    } else if ((playerSelection === "scissors" || computerSelection === "scissors") && (playerSelection === "rock" || computerSelection === "rock")) {
-        if (playerSelection === "rock") {
+    } else if ((playerSelection === btns[2] || computerSelection === "scissors") && (playerSelection === btns[0] || computerSelection === "rock")) {
+        if (playerSelection === btns[0]) {
             return "You win! Rock beats scissors";
         } else {
             return "You lose! Rock beats scissors";
         }
-    } else {
-        return "Wrong input";
-    }
-    
-}
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = computerPlay();
-        let playerSelection = prompt("Rock, paper or scissors?");
-        playerSelection = playerSelection.toLowerCase();
-        console.log(playRound(playerSelection, computerSelection));
     }
 }
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+//         let computerSelection = computerPlay();
+        
+//     }
+// }
 
-game();
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(btn => btn.addEventListener("click", function(){
+    playerSelection = btn;
+    return playerSelection;
+}))
+
